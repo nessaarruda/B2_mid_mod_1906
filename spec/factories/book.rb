1429ce074817f0f95c4018bbdc82594891ac250b
Vanessa Arruda <wanessaarruda@gmail.com>
2020-12-29 16:34:05 -0700

    Finish models setup and migrations

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :book do
     title { Faker::Book.title}
+    publication_year { Faker::Number.within(range: 1900..2020)}
+    number_of_pages { Faker::Number.within(range: 100..500)}
   end
 end
