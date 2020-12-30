58ec811a11f84aa4984c62c78128d4a9611b3c89
Vanessa Arruda <wanessaarruda@gmail.com>
2020-12-29 17:08:03 -0700

    Pass story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+AuthorBook.destroy_all
+Author.destroy_all
+Book.destroy_all
+
+@book_1 = Book.create!(title: "The Perks of Being a Wallflower", number_of_pages: 300, publication_year: 1965)
+@author_1 = Author.create!(name: "Michelle Randolph")
+@book_2 = Book.create!(title: "O Vento Levou", number_of_pages: 200, publication_year: 1945)
+@author_2 = Author.create!(name: "Vanessa Arruda")
+
+AuthorBook.create!(author_id: @author_1.id, book_id: @book_1.id)
+AuthorBook.create!(author_id: @author_2.id, book_id: @book_2.id)
+AuthorBook.create!(author_id: @author_2.id, book_id: @book_1.id)
+AuthorBook.create!(author_id: @author_1.id, book_id: @book_2.id)
